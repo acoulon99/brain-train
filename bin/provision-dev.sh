d417c20e4a78bf61b48b306bb48a41a0e16ca209
Adam Coulon <adamgcoulon@gmail.com>
2016-11-12 18:17:57 -0600

    small updates

--- bin/provision-dev.sh
+++ bin/provision-dev.sh
@@ -28,9 +28,6 @@ sudo npm install pm2 -g
 rsync -avh --include=".gitignore" --exclude="node_modules" --exclude=".*" --delete "$SYNCED_FOLDER"/ "$PROJECT_HOME"
 chmod u+x $BIN/*.sh # make scripts executable for Vagrant user
 
-
-chmod u+x $BIN/*.sh
-
 # Install node dependencies
 npm install
 
