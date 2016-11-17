36dc16a4783c3881e7edcc3cda6e5af6e44d773b
Adam Coulon <adamgcoulon@gmail.com>
2016-11-16 21:27:46 -0600

    dev work

--- bin/sync.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Syncronizes project on vagrant with host
-
-PROJECT_HOME=/home/vagrant
-SYNCED_FOLDER=/vagrant
-BIN=$PROJECT_HOME/bin
-
-rsync -avh --include=".gitignore" --exclude="node_modules" --exclude=".*" --delete "$SYNCED_FOLDER"/ "$PROJECT_HOME"
-chmod u+x $BIN/*.sh # make scripts executable for Vagrant user
